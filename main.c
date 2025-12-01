3964230fd64f6c3fcbe677afc950fc5071a5d749
Al3r4b <abdalah2003hu@gmail.com>
2025-12-01 13:48:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+
+void calc(float param1, float param2, char operator){
+
+float resultat;
+int resultat1;
+
+switch (operator) {
+case '+':       resultat1 = param1 + param2;
+ printf("%d\n", resultat1);
+ break;
+case'-':        resultat1 = param1 - param2;
+ printf("%d\n", resultat1);
+ break;
+case'*':        resultat1 = param1 * param2;
+ printf("%d\n", resultat1);
+ break;
+case'/':       if(param2 == 0){
+    printf("NaN\n"); break;
+ } else{ resultat = param1 / param2;
+    printf("%f\n", resultat); break;
+ }
+case'^':        resultat1= pow( param1,param2);
+    printf("%d\n", resultat1);
+ break;
+}
+}
+int main(){
+float a,b;
+char operation;
+
+scanf("%f", &a);
+
+scanf(" %c", &operation);
+
+scanf("%f", &b);
+
+calc(a, b, operation);
+
+return 0;
+}
 
