0991736b65a35c25e1cbca459fde965a03615526
Sury <46917227+Suryyartz@users.noreply.github.com>
2025-12-01 14:12:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("1. Add Vehicle\n
-2. Remove Vehicle\n
-3. Sort\n
-4. Show Vehicle\n
-5. Show Registry\n
-0. Quit!\n");
+    printf("1. Add Vehicle\n"
+           "2. Remove Vehicle\n"
+           "3. Sort\n"
+           "4. Show Vehicle\n"
+           "5. Show Registry\n"
+           "0. Quit!\n");
     return 0;
 }
