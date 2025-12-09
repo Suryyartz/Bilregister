7034b5c38c68b82e83a175f943ed856e3010ba2b
Sury <46917227+Suryyartz@users.noreply.github.com>
2025-12-09 17:03:37 +0100

    Update fil.h

--- fil.h
+++ fil.h
@@ -1,9 +1,5 @@
-//Library
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <ctype.h>
+#ifndef FIL_H
+#define FIL_H
 
 //Define
 #define Add_vehicle 1
@@ -15,5 +11,35 @@
 #define Quit 0
 #define Size 10
 #define Name 256
+#define Max_line 60
 #define ANSI_COLOR_BLUE  "\x1b[34m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define Object 1
+
+//lista
+typedef struct{
+ char name [Name];
+ int age;
+} Person;
+
+typedef struct{
+    char Brand[Name];
+    char Model[Name];
+    char Reg_number[Name];
+    Person Owner;
+} Vehicle;
+
+void init_reg (Vehicle *reg, int size);
+
+//Add/Remove
+void Addvehicle (Vehicle *reg, int *position);
+//registry
+void Showreg (Vehicle *reg, int position);
+
+
+//Svae/Load
+void Save_file (Vehicle *reg, int position);
+void Load_file (Vehicle *reg, int *position);
+
+
+#endif
